d3b42a4fda7fd6e494305b42edd63c753045520e
Luke Stamper <luke.stamper12@outlook.com>
2022-11-29 16:24:08 -0700

    added two classes

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,42 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+let game = {
+	gridSize: 20,
+	refreshRate: 500, // Milliseconds
+};
+
+class Cobra {
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 */
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+		this.w = game.gridSize;
+		this.h = game.gridSize;
+		this.segments = [];
+	}
+}
+
+class segment {
+	/**
+	 * @param {number} x
+	 * @param {number} y
+	 * @param {string} color
+	 */
+	constructor(x, y, color) {
+		this.x = x;
+		this.y = y;
+		this.w = game.gridSize;
+		this.h = game.gridSize;
+		this.segments = [];
+		this.color = color;
+	}
+
+	update() {}
+
+	draw() {}
+}
